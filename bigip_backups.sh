17c5ec4c808fc3f3a96a9800cfa5d9de15beb038
Riley Schuit <riley.schuit@gmail.com>
2014-05-20 17:52:13 -0700

    new file:   bigip_backups.sh

---
+++ bigip_backups.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Todays Date:
+DATE=`date +"%m-%d-%y"`
+mkdir -p ~/backups/
+
+# Make and array of your bigips
+declare -A BIGIPS
+BIGIPS[GTM01]="172.24.130.86"
+BIGIPS[GTM02]="172.24.130.87"
+BIGIPS[LTM01]="172.24.130.88"
+BIGIPS[LTM02]="172.24.130.89"
+
+if [ "$1" == "setup" ]; then
+  ssh-keygen -t rsa
+  for i in "${!BIGIPS[@]}"
+  do
+    cat .ssh/id_rsa.pub | ssh root@${BIGIPS[$i]} 'cat >> /root/.ssh/authorized_keys'
+  done
+fi
+
+  for i in "${!BIGIPS[@]}"
+  do
+    ssh root@${BIGIPS[$i]} -t 'tmsh save sys ucs /var/local/ucs/$HOSTNAME.`date +"%m-%d-%y"`.ucs'
+    scp root@${BIGIPS[$i]}:/var/local/ucs/*`date +"%m-%d-%y"`.ucs ~/backups/
+  done
+
+# delete UCSs that are three weeks old.
+find ~/backups/ -type f  -mtime +21 -exec rm {} \;
